4bfc21334749ef4fa5d3fc9db24b70cceff4657f
Farid Ibrahimzada <ibrahimzadef26@gmail.com>
2023-01-13 19:36:58 +0400

    adding

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
